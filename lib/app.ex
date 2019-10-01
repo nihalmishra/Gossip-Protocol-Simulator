2ee05c9c2c3950fa9ee874e325416006868a1bf7
nihalmishra <nihal.mishra95@gmail.com>
2019-10-01 11:56:01 -0400

    update gossip module

--- lib/app.ex
+++ lib/app.ex
@@ -7,6 +7,8 @@ defmodule App do
     peer_list = Enum.to_list(1..num_peer)
     create_gossip_peer(peer_list)
     start_time = System.monotonic_time(:millisecond)
+    time_table = :ets.new(:time_table, [:set, :public, :named_table])
+    :ets.insert(time_table, {"count",0})
     start_gossip(peer_list, start_time)
   end
 
@@ -17,14 +19,14 @@ defmodule App do
   def create_gossip_peer(peer_list) do
     # msg_seen = 0
     Enum.map peer_list, fn peer ->
-      {:ok, pid} = GenServer.start_link(GossipPeer, %{msg_seen: 0, name: peer, total_nodes: length(peer_list)}, name: via_tuple(Integer.to_string(peer)))
-      IO.inspect(pid)
+      {:ok, pid} = GenServer.start_link(GossipPeer, %{msg_seen: 0, name: peer, gossip_mode: false, total_nodes: length(peer_list)}, name: via_tuple(Integer.to_string(peer)))
+      # IO.inspect(pid)
     end
   end
 
   def start_gossip(peer_list,start_time) do
     IO.puts "starting gossip"
-    GenServer.cast(via_tuple(Integer.to_string(Enum.at(peer_list,0))),{:gossip,start_time})
+    GenServer.cast(via_tuple(Integer.to_string(Enum.at(peer_list,0))),{:receive,start_time})
     # Helper.span(peer_list, start_time)
   end
 
